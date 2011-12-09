83b6f572835f58c238c30f16cad1878bc41dbe16
Thomas Spycher <tspycher@zero-one.ch>
2011-12-09 11:20:16 +0100

    fixed dialog issue

--- init.mod.sh
+++ init.mod.sh
@@ -5,10 +5,10 @@ OUTPUT=/tmp/output.sh.$$
 
 trap "rm $OUTPUT; rm $INPUT; exit" SIGHUP SIGINT SIGTERM
 
-DIALOG=`whereis dialog`
-if [ ! $DIALOG ]; then 
+DIALOG=`whereis -b dialog | awk '{ print $2}'`
+if [ ! "$DIALOG" ]; then 
     DIALOG=`find /opt -name dialog -print | grep bin 2> /dev/null`
-    if [ ! $DIALOG ]; then 
+    if [ ! "$DIALOG" ]; then 
         apt-get -y install dialog 
     fi
 fi
