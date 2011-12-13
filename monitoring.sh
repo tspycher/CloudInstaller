76126f5bb6ab5ac9d6f60ccb1d18627556dda393
Thomas Spycher <tspycher@zero-one.ch>
2011-12-13 23:12:37 +0100

    added monitoring script

---
+++ monitoring.sh
@@ -0,0 +1,25 @@
+dialog --no-kill --no-ok \
+--begin 5 5 --keep-window --infobox "test" 10 10 \
+--and-widget --keep-window --no-kill \
+--begin 20 20 --msgbox "test" 10 10
+
+
+
+
+#  [--ascii-lines] [--aspect <ratio>] [--backtitle <backtitle>]
+#  [--begin <y> <x>] [--cancel-label <str>] [--clear] [--colors]
+#  [--column-separator <str>] [--cr-wrap] [--date-format <str>]
+#  [--default-item <str>] [--defaultno] [--exit-label <str>]
+#  [--extra-button] [--extra-label <str>] [--help-button]
+#  [--help-label <str>] [--help-status] [--hfile <str>] [--hline <str>]
+#  [--ignore] [--input-fd <fd>] [--insecure] [--item-help] [--keep-tite]
+#  [--keep-window] [--max-input <n>] [--no-cancel] [--no-collapse]
+#  [--no-kill] [--no-label <str>] [--no-lines] [--no-mouse]
+#  [--no-nl-expand] [--no-ok] [--no-shadow] [--nook] [--ok-label <str>]
+#  [--output-fd <fd>] [--output-separator <str>] [--print-maxsize]
+#  [--print-size] [--print-version] [--quoted] [--scrollbar]
+#  [--separate-output] [--separate-widget <str>] [--shadow]
+#  [--single-quoted] [--size-err] [--sleep <secs>] [--stderr] [--stdout]
+#  [--tab-correct] [--tab-len <n>] [--time-format <str>] [--timeout <secs>]
+#  [--title <title>] [--trace <file>] [--trim] [--version] [--visit-items]
+#  [--yes-label <str>]
